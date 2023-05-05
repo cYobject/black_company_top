f69153be8ab07691ff9be07d471b7b65e1a6d309
jax <5651840@qq.com>
2023-05-06 00:17:46 +0800

    init

---
+++ src/main/java/com/cyobject/black_company_top/BlackCompanyTopApplication.java
@@ -0,0 +1,13 @@
+package com.cyobject.black_company_top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackCompanyTopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackCompanyTopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyobject/black_company_top/BlackCompanyTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyobject.black_company_top;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackCompanyTopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
